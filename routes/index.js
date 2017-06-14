e8989aecf936f9d6f5e072bd222b8e0e1e8d52a7
Yuriy Ivanov <ymivanov@icloud.com>
2017-06-14 01:14:59 -0400

    setting new routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page.
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+*/
+
+router.get('/', function(request, response) {
+  response.render('index', {
+    title: "Yuriy",
+    age: "30"
+  });
+
+  console.log(request.query)
+});
+
+
+
+
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/:id', function(request, response) {
+
+  response.sendStatus(200);
+  response.send({
+    "userId": request.params.id
+  });
+  console.log(request.params);
+});
+
+
+
 
 module.exports = router;
